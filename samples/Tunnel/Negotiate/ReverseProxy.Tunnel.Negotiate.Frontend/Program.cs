758dd3917c56ac49a9c8efe6d8004db45bc02123
Florian Grimm <grimmuko@gmail.com>
2024-10-18 00:56:09 +0200

    step

--- samples/Tunnel/Negotiate/ReverseProxy.Tunnel.Negotiate.Frontend/Program.cs
+++ samples/Tunnel/Negotiate/ReverseProxy.Tunnel.Negotiate.Frontend/Program.cs
@@ -1,15 +1,55 @@
+#if README
+Create a AppRegistration in Azure AD and update the configuration in appsettings.json or user secrets.
+You need the Application ID URI. It should be in the format `api://{ClientId}`.
+{
+  "AzureAd": {
+    "Instance": "https://login.microsoftonline.com/",
+    "ClientId": "....",
+    "TenantId": "....",
+    "Audience": "api://{ClientId}",
+    "ClientSecret": "...."
+  }
+}
+
+--------------------------------
+| Browser                      |
+| https://localhost:5001/index |
+--------------------------------
+            |           ^
+            |           |
+            v           |
+--------------------------------
+| ReverseProxy.Tunnel.Frontend |
+| https://localhost:5001/      |
+--------------------------------
+        |     ||     /\
+        |     ||     ||
+        ^     \/     ||
+--------------------------------
+| ReverseProxy.Tunnel.Backend  |
+| https://localhost:5003/      |
+--------------------------------
+                 |  ^
+                 |  |
+                 v  |
+--------------------------------
+| ReverseProxy.Tunnel.API      |
+| https://localhost:5005/      |
+--------------------------------
+
+#endif
+
 using Microsoft.AspNetCore.Authentication.Negotiate;
 
-internal class Program
+using Yarp.ReverseProxy.Tunnel;
+
+namespace ReverseProxy.Tunnel.Frontend;
+
+public class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-
         builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
            .AddNegotiate();
 
@@ -19,15 +59,22 @@ internal class Program
             options.FallbackPolicy = options.DefaultPolicy;
         });
 
-        var app = builder.Build();
+        builder.Configuration.AddUserSecrets("ReverseProxy");
+        builder.Logging.AddLocalFileLogger(builder.Configuration, builder.Environment);
+        var reverseProxyBuilder = builder.Services.AddReverseProxy()
+            .LoadFromConfig(builder.Configuration.GetRequiredSection("ReverseProxy"))
+            .AddTunnelServices() // enable tunnel listener
+            .AddTunnelServicesNegotiate()
+            ;
 
-        // Configure the HTTP request pipeline.
+        var app = builder.Build();
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
-        app.MapControllers();
+        app.MapReverseProxy();
 
         app.Run();
     }
